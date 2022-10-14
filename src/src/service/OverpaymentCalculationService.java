e5a14a0ec9159004e229eb9512c4f2f5a96df935
Artur <artur.augustyn26@gmail.com>
2022-10-14 20:31:23 +0200

    added full printing method

---
+++ src/src/service/OverpaymentCalculationService.java
@@ -0,0 +1,11 @@
+package service;
+
+import model.InputData;
+import model.Overpayment;
+
+import java.math.BigDecimal;
+
+public interface OverpaymentCalculationService {
+
+     Overpayment calculate(final BigDecimal rateNumber, final InputData inputData);
+}
